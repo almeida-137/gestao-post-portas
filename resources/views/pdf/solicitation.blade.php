811b9258eb00f1c00e535cfc1a867ae935920b49
Mateus Almeida <almeida.postportas@gmail.com>
2024-11-18 10:52:10 -0300

    pdf

--- resources/views/pdf/solicitation.blade.php
+++ resources/views/pdf/solicitation.blade.php
@@ -111,6 +111,7 @@
                 <th>Ambiente</th>
                 <th>Quantidade</th>
                 <th>Descrição</th>
+                <th>Dimensões</th>
                 <th>Cor</th>
                 <th>Cor Borda</th>
                 <th>Observação</th>
@@ -119,7 +120,8 @@
             <tr>
                 <td class='itens'>{{ $peca['ambiente'] }}</td>
                 <td class='itens'>{{ $peca['quantidade'] }}</td>
-                <td class='itens'>{{ $peca['descricao'] }} - {{ $peca['dimensoes']['largura'] }}x{{ $peca['dimensoes']['altura'] }}x{{ $peca['dimensoes']['profundidade'] }}</td>
+                <td class='itens'>{{ $peca['descricao'] }}</td>
+                <td class='itens'>{{ $peca['dimensoes']['largura'] }}x{{ $peca['dimensoes']['altura'] }}x{{ $peca['dimensoes']['profundidade'] }}</td>
                 <td class='itens'>{{ $peca['cor'] }}</td>
                 <td class='itens'>{{ $peca['cor_borda'] }} - {{$peca['filetacao']}}</td>
                 <td class='itens'>{{ $peca['obs'] }}</td>
